10b26bf836e48100e9818d911f7794e392ec5a12
gsmanivel@gmail.com <gsmanivel@gmail.com>
2021-12-07 20:23:57 -0500

    updates

--- app.py
+++ app.py
@@ -7,8 +7,7 @@ from aws_cdk import aws_s3 as _s3
 
 from cdk_python.cdk_python_stack import CdkPythonStack
 
-env_US = core.Environment(account="081920805510", region="us-east-1")
-
+ 
 app = core.App()
-CdkPythonStack(app, "first-cdk-stack-us", env=env_US)
+CdkPythonStack(app, "first-cdk-stack-us")
 app.synth()
