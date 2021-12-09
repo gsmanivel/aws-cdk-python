24d9084ddfbbfbb345f08d76fe9365618c65b112
gsmanivel@gmail.com <gsmanivel@gmail.com>
2021-12-09 00:11:47 -0500

    added output

--- cdk_python/cdk_python_stack.py
+++ cdk_python/cdk_python_stack.py
@@ -7,12 +7,33 @@ class CdkPythonStack(cdk.Stack):
     def __init__(self, scope: cdk.Construct, construct_id: str, **kwargs) -> None:
         super().__init__(scope, construct_id, **kwargs)
 
-        # The code that defines your stack goes here
-        _s3.Bucket(
+        # create a db2 bucket
+        db2bucket = _s3.Bucket(
+                self,
+                "db2bucket",
+                bucket_name="db2bucket001",
+                versioned=True,
+                encryption=_s3.BucketEncryption.S3_MANAGED,
+                block_public_access= _s3.BlockPublicAccess.BLOCK_ALL,
+                removal_policy=core.RemovalPolicy.DESTROY,
+                auto_delete_objects= True
+                )
+
+        # create a mysqlbucket
+        mysqlbucket = _s3.Bucket(
             self,
-            "myFirstcdkBucket2021",
-            bucket_name="myfirstcdk272002",
-            versioned=False,
-            encryption=_s3.BucketEncryption.S3_MANAGED,
-            block_public_access= _s3.BlockPublicAccess.BLOCK_ALL
-            )
+            "mysqlbucket001",
+            bucket_name="mysqlbucket001" ,
+            removal_policy= core.RemovalPolicy.DESTROY,
+            auto_delete_objects=True,
+            block_public_access=_s3.BlockPublicAccess.BLOCK_ALL,
+                       
+        )
+
+        bucketOutput_db2 = core.CfnOutput(
+            self,
+            "db2bucketoutput001",
+            value= db2bucket.bucket_name,
+            description= f"db2bucket.bucket_arn",
+            export_name="db2outputdetails"
+        )
